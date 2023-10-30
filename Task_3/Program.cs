824ae49af26d1137cf87d10dd9b9539bbf88e9d2
Konstantin Solovei <SoloveiKonstantin@gmail.com>
2023-10-30 19:52:57 +0300

    Solution Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine($"[{String.Join(", ", GetArray(GetNumber()))}]");
+
+int GetNumber()
+{
+    int num;
+    while (true)
+    {
+        Console.Write($"Введите количество элементов массива: ");
+        string stringNumber = Console.ReadLine();
+        bool isNumber = int.TryParse(stringNumber, out num);
+        if (isNumber)
+        {
+            Console.WriteLine($"Вы ввели число: {num}.");
+            break;
+        }
+        else Console.WriteLine("Вы ввели не число, а текст!");
+    }
+    return num;
+}
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0, 1000);
+    }
+    return array;
+}
